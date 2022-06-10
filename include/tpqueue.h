c116718218cb4ad94593e180b1efde9783e751b0
CheginArtyom <99319275+CheginArtyom@users.noreply.github.com>
2022-06-10 19:11:38 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,13 +3,55 @@
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
 
-struct SYM {
+struct DATA {
   char ch;
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+class TPQueue {
+ private:
+ ITEM* head;
+  struct ITEM {
+    T data;
+    ITEM* next;
+  };
+  TPQueue::ITEM* create(const T& znach) {
+    ITEM* uk = new ITEM;
+    uk->data = znach;
+    uk->next = nullptr;
+    return uk;
+  }
+  
+ public:
+  TPQueue() :head(nullptr) {}
+  T pop() {
+    ITEM* temp = head;
+    head = head->next;
+    return temp->data;
+  }
+  void push(T znach) {
+    if (head) {
+      ITEM* th = create(znach);
+      if (znach.prior > head->data.prior) {
+        th->next = head;
+        head = th;
+        return;
+      }
+      ITEM* temp = head;
+      while (temp->next) {
+      if (znach.prior > temp->next->data.prior) {
+        th = create(znach);
+        th->next = temp->next;
+        temp->next = th;
+        return;
+      }
+        temp = temp->next;
+    }
+    temp->next = create(znach);
+    } else {
+      head = create(znach);
+      return;
+    }
+  }
+};
